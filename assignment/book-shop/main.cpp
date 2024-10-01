590a6cde789e98ee1d18430da72bf676bc8a5de9
Suraj Biswas <surajbiswas182003@gmail.com>
2024-10-01 17:28:31 +0530

    added

--- assignment/book-shop/main.cpp
+++ assignment/book-shop/main.cpp
@@ -7,10 +7,8 @@ using namespace std;
 class book
 {
 private:
-    char *author;
-    char *title;
+    char *author, *title, *publisher;
     int price;
-    char *publisher;
     int stock;
 
 public:
@@ -30,31 +28,35 @@ public:
         this->stock = stock;
     }
 
-    char *getAuthor()
+    char* get_author()
     {
         return this->author;
     }
-    char *getTitle()
+    char* get_title()
     {
         return this->title;
     }
-    int getPrice()
+    int get_price()
     {
         return this->price;
     }
-    char *getPublisher()
+    char* get_publisher()
     {
         return this->publisher;
     }
-    int getStock()
+    int get_stock()
     {
         return this->stock;
     }
 
+    void set_stock(int stock) {
+        this->stock = stock;
+    }
+
     void show_details()
     {
-        cout << "Author: " << author << "\n";
         cout << "Title: " << title << "\n";
+        cout << "Author: " << author << "\n";
         cout << "Price: " << price << "\n";
         cout << "Publisher: " << publisher << "\n";
     }
@@ -80,6 +82,7 @@ public:
         book *b3 = new book("yashwant kanitkar", "let us c", 350, "laxmi", 20);
         add(b1);
         add(b2);
+        add(b3);
     }
 
     void add(book *b)
@@ -91,13 +94,14 @@ public:
     {
         char *author = new char[100];
         char *title = new char[100];
-        cout << "Author: ";
-        cin.getline(author, 100);
         cout << "Title: ";
         cin.getline(title, 100);
+        cout << "Author: ";
+        cin.getline(author, 100);
 
         if (is_available(author, title))
         {
+            cout << "\n***Book is Available***\n\n";
             book *b = get_book(author, title);
             b->show_details();
             int required_copies;
@@ -107,6 +111,7 @@ public:
             {
                 int total = total_price(b, required_copies);
                 cout <<"Total price: " << total << endl;
+                b->set_stock(b->get_stock() - required_copies);
             }
             else {
                 cout << "The required copies not in stock\n";
@@ -126,7 +131,7 @@ public:
     {
         for (auto book : books)
         {
-            if (strcmp(book->getAuthor(),author) == 0 && strcmp(book->getTitle(), title) == 0)
+            if (strcmp(book->get_author(),author) == 0 && strcmp(book->get_title(), title) == 0)
             {
                 return book;
             }
@@ -138,7 +143,7 @@ public:
     {
         for (auto book : books)
         {
-            if (strcmp(book->getAuthor(),author) == 0 && strcmp(book->getTitle(), title) == 0)
+            if (strcmp(book->get_author(),author) == 0 && strcmp(book->get_title(), title) == 0)
             {
                 return true;
             }
@@ -148,19 +153,19 @@ public:
 
     bool is_in_stock(book *b, int required_copies)
     {
-        return required_copies <= b->getStock();
+        return required_copies <= b->get_stock();
     }
 
     int total_price(book *b, int copies)
     {
-        return b->getPrice() * copies;
+        return b->get_price() * copies;
     }
 
     void get_books()
     {
         for (auto &book : books)
         {
-            cout << "Author: " << book->getAuthor() << " and Title: " << book->getTitle() << endl;
+            cout << "Author: " << book->get_author() << " and Title: " << book->get_title() << endl;
         }
     }
 
@@ -176,8 +181,7 @@ public:
 int main()
 {
     BookInventory inventory;
-    for (int i = 0; i < 2; i++)
-        inventory.search();
+    inventory.search();
 
     return 0;
 }
