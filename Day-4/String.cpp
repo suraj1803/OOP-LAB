cd051bbc562cafd84d0493e4d755a046d1bbb587
Suraj Biswas <surajbiswas182003@gmail.com>
2024-10-08 22:00:20 +0530

    modified

--- Day-4/String.cpp
+++ Day-4/String.cpp
@@ -2,36 +2,45 @@
 #include <string.h>
 using namespace std;
 
-class String {
+class String
+{
 private:
     char *str;
+
 public:
-    String(const char* s) {
+    String(const char *s)
+    {
         str = new char[strlen(s) + 1];
         strcpy(str, s);
     }
 
-    /* bool operator==(const String &s){ */
-    /*     return strcmp(this->str, s.str) == 0; */
-    /* } */
+    bool operator==(const String &s)
+    {
+        return strcmp(this->str, s.str) == 0;
+    }
 
     friend bool operator==(const String &s1, const String &s2);
 
-    ~String() {
+    ~String()
+    {
         delete[] str;
     }
 };
 
-bool operator==(const String &s1, const String &s2) {
+bool operator==(const String &s1, const String &s2)
+{
     return strcmp(s1.str, s2.str) == 0;
 }
 
-int main() {
+int main()
+{
     String s1("hi"), s2("hii");
-    if (s1 == s2) {
+    if (s1 == s2)
+    {
         cout << "Strings are equal\n";
     }
-    else {
+    else
+    {
         cout << "Strings are not equal\n";
     }
 
